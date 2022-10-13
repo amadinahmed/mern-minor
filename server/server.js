6b2144563b3bcbe461430e7ce8e674a010cc2dc4
Amin Ahmed <48972428+aminahmed65@users.noreply.github.com>
2022-10-13 10:53:38 -0400

    deployment

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const pdfParse = require("pdf-parse");
+const fileUpload = require("express-fileupload");
+
+const app = express();
+
+
+app.get("/api",(req,res) =>{
+    res.json({"users":['1','2','3']})
+})
+
+app.use("/file", express.static("public"));
+app.use(fileUpload());
+
+app.post("/extract-text", (req, res) => {
+    if (!req.files && !req.files.pdfFile) {
+        res.status(400);
+        res.end();
+    }
+
+    pdfParse(req.files.pdfFile).then(result => {
+        res.send(result.text);
+    });
+});
+
+app.listen(5000,() => {console.log("Server port : 5000")})
