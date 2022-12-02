1a5c547d2a8c3712886313d8da776b36214aeb17
Amin Ahmed <48972428+aminahmed65@users.noreply.github.com>
2022-12-01 19:43:31 -0500

    Created Minor Report

--- client/src/App.js
+++ client/src/App.js
@@ -213,27 +213,6 @@ function App() {
 
   return (
     <div>
-      
-      {(typeof backendData.users === 'undefined') ? (<p>Loading...</p>)
-      : 
-      (
-        backendData.users.map((user,i) => (
-          <p key={i}>{user}</p>
-
-        ))
-      )
-
-      }
-    
-      <Req_api/>
-      
-      
-
-      <div className="card-body">
-                Returned Id: {articleId}
-            </div>
-     
-    
 
       <Router>
       <div className="Main">
@@ -279,21 +258,22 @@ function App() {
       <div className=" modal-dialog modal-dialog-centered modal-dialog-scrollable relative w-auto pointer-events-none">
         <div className="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current">
           <div className="modal-header flex flex-shrink-0 items-center justify-between p-4 border-b border-gray-200 rounded-t-md">
-            <h5 className="text-xl font-medium leading-normal text-gray-800" id="exampleModalCenteredScrollableLabel">
-            Upload Academic Report
-            </h5>
-            <button type="button"
-              className="pr-3 btn-close box-content w-4 h-4 p-1 text-black border-none rounded-none focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline"
-              data-bs-dismiss="modal" aria-label="Close"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"  class="w-6 h-6">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-            </svg>
-            </button>
-          </div>
-          <div className="modal-body relative p-4">
-
+           
+                <h5 className="text-xl font-medium leading-normal text-gray-800" id="exampleModalCenteredScrollableLabel">
+                  Upload Academic Report
+                  </h5>
+                  <button type="button"
+                    className="pr-3 btn-close box-content w-4 h-4 p-1 text-black border-none rounded-none focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline"
+                    data-bs-dismiss="modal" aria-label="Close"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"  class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                  </svg>
+                  </button>
+                </div>
+                <div className="modal-body relative p-4">
+         
            {/* handles user upload data */}
 
-        <div>
+        <div id='report'>
           <form id="form" onSubmit={handleSubmit}>
             <div className='pb-10 mx-4'>
                 <textarea className='
@@ -333,23 +313,22 @@ function App() {
             
         
 
-        <p>Just like that.</p>
-
         </div>
 
-          <div
-            className="modal-footer flex flex-shrink-0 flex-wrap items-center justify-end p-4 border-t border-gray-200 rounded-b-md">
-            <button type="button"
-              className=" inline-block px-6 py-2.5 mr-3 bg-gray-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-purple-800 active:shadow-lg transition duration-150 ease-in-out"
-              data-bs-dismiss="modal">
-              Close
+        
+          <button class="inline-block px-6 py-4 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg  focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out " type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasBottom" aria-controls="offcanvasBottom">
+            Run Report
             </button>
-            <button type="button"
-              className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out ml-1">
-              Continue
-            </button>
-          </div>
 
+          <div class="offcanvas offcanvas-bottom fixed bottom-0 flex flex-col max-w-full bg-white invisible bg-clip-padding shadow-sm outline-none transition duration-300 ease-in-out text-gray-700 left-0 right-0 border-none h-4/5 max-h-full" tabindex="-1" id="offcanvasBottom" aria-labelledby="offcanvasBottomLabel">
+            <div class="offcanvas-header flex items-center justify-between p-4">
+              <h5 class="offcanvas-title mb-0 leading-normal font-semibold" id="offcanvasBottomLabel">Certifications / Minor Report</h5>
+              <button type="button" class="btn-close box-content w-4 h-4 p-2 -my-5 -mr-2 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body flex-grow p-4 overflow-y-auto small">
+              <Req_api/>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- client/src/components/api.js
+++ client/src/components/api.js
@@ -11,7 +11,7 @@ export class api extends Component {
       
 
       this.state = {
-         values: 'Loading ...'
+        afam_minor: ''
       }
    }
 
@@ -26,7 +26,9 @@ export class api extends Component {
    console.log(this.state.title)
   }
 
-  api_function = (e) => {
+   
+  // Minor AFAM,
+  afam_minor_function = (e) => {
    e.preventDefault()
    
      
@@ -40,28 +42,68 @@ export class api extends Component {
        ).then(response => {
          if (response.ok) {
            response.json().then(json => {
-             console.log(json);
+            
+             // Adding spaces in Array output
+   
+             let array1_joined = json[1].join(', ');
+            
+             json[1] = array1_joined
+
+             let array2_joined = json[5].join(', ');
+            
+             json[5] = array2_joined
+
+             let array3_joined = json[7].join(', ');
+            
+             json[7] = array3_joined
+
+            
+
+             json[2] = [json[2],' ', json[3], ' ', json[4]];
+
+             json[3] = ''
+             json[4] = ''
+             json.splice(3, 4);
+         
+
+             let arrayLength = json.length;
+             for (let i = 0; i < arrayLength; i++) {
+                 console.log(json[i]);
+                 //Do something
+             }
+
              this.setState({
-              values: json
+              afam_minor: json
             });
            });
          }
        });
-  }
-
+    }
+  
 
 
   render() {
-   const {values} = this.state
     return (
       <div>
-            <form onSubmit={this.api_function}>
-    
+          
+          <div id='AFAM Minor'>
+            <h1 className='font-medium leading-tight text-3xl mt-0 mb-2 text-blue-600' >AFAM Minor</h1>
+            {Object.entries(this.state.afam_minor).map(([key, value]) => (
+              <tr key={key}>
+                <br/>
+                  <td><br/>{value}</td>
+              </tr>
+          ))}
+          </div>
+
+          
+          <form onSubmit={this.afam_minor_function}>
             <div>
-               <button type='submit'>Some</button>
-               </div>
-            </form>
-            <p>{values}</p>
+              <button type='submit' class="mt-3 mb-2  w-full inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-normal uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
+              Run Report
+              </button>
+            </div>
+          </form>
       </div>
     )
   }
